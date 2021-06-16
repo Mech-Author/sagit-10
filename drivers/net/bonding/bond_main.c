a9ce5f14e51f9f5434671c9232a82fd846e9f0f7
Greg Kroah-Hartman <gregkh@google.com>
2021-06-16 12:05:35 +0200

    Merge 4.4.273 into android-4.4-p
    Changes in 4.4.273
    	proc: Track /proc/$pid/attr/ opener mm_struct
    	net/nfc/rawsock.c: fix a permission check bug
    	ASoC: sti-sas: add missing MODULE_DEVICE_TABLE
    	isdn: mISDN: netjet: Fix crash in nj_probe:
    	bonding: init notify_work earlier to avoid uninitialized use
    	netlink: disable IRQs for netlink_lock_table()
    	net: mdiobus: get rid of a BUG_ON()
    	net/qla3xxx: fix schedule while atomic in ql_sem_spinlock
    	scsi: vmw_pvscsi: Set correct residual data length
    	scsi: target: qla2xxx: Wait for stop_phase1 at WWN removal
    	net: macb: ensure the device is available before accessing GEMGXL control registers
    	net: appletalk: cops: Fix data race in cops_probe1
    	MIPS: Fix kernel hang under FUNCTION_GRAPH_TRACER and PREEMPT_TRACER
    	bnx2x: Fix missing error code in bnx2x_iov_init_one()
    	powerpc/fsl: set fsl,i2c-erratum-a004447 flag for P2041 i2c controllers
    	powerpc/fsl: set fsl,i2c-erratum-a004447 flag for P1010 i2c controllers
    	i2c: mpc: Make use of i2c_recover_bus()
    	i2c: mpc: implement erratum A-004447 workaround
    	kvm: avoid speculation-based attacks from out-of-range memslot accesses
    	btrfs: return value from btrfs_mark_extent_written() in case of error
    	cgroup1: don't allow '\n' in renaming
    	USB: f_ncm: ncm_bitrate (speed) is unsigned
    	usb: dwc3: ep0: fix NULL pointer exception
    	USB: serial: ftdi_sio: add NovaTech OrionMX product ID
    	USB: serial: omninet: add device id for Zyxel Omni 56K Plus
    	USB: serial: quatech2: fix control-request directions
    	usb: gadget: eem: fix wrong eem header operation
    	perf: Fix data race between pin_count increment/decrement
    	NFS: Fix a potential NULL dereference in nfs_get_client()
    	perf session: Correct buffer copying when peeking events
    	kvm: fix previous commit for 32-bit builds
    	NFSv4: nfs4_proc_set_acl needs to restore NFS_CAP_UIDGID_NOMAP on error.
    	scsi: core: Only put parent device if host state differs from SHOST_CREATED
    	ftrace: Do not blindly read the ip address in ftrace_bug()
    	proc: only require mm_struct for writing
    	Linux 4.4.273
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I97fdaeb60b62a57bf34ecceabda8be5cee23a0e7

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1233,6 +1233,13 @@ static struct slave *bond_alloc_slave(struct bonding *bond)
 	if (!slave)
 		return NULL;
 
+	slave->bond = bond;
+	slave->dev = slave_dev;
+	INIT_DELAYED_WORK(&slave->notify_work, bond_netdev_notify_work);
+
+	if (bond_kobj_init(slave))
+		return NULL;
+
 	if (BOND_MODE(bond) == BOND_MODE_8023AD) {
 		SLAVE_AD_INFO(slave) = kzalloc(sizeof(struct ad_slave_info),
 					       GFP_KERNEL);
@@ -1241,7 +1248,6 @@ static struct slave *bond_alloc_slave(struct bonding *bond)
 			return NULL;
 		}
 	}
-	INIT_DELAYED_WORK(&slave->notify_work, bond_netdev_notify_work);
 
 	return slave;
 }
